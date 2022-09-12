105bc6a8e8d453715139d5d8fcd822eae5993f31
90090446 <1482795788@qq.com>
2022-09-12 21:34:17 +0800

    test_zz

--- java/main/zz/git/TestRebase.java
+++ java/main/zz/git/TestRebase.java
@@ -3,7 +3,9 @@ package zz.git;
 public class TestRebase {
 
     public static void main(String[] args) {
-        System.out.println("test main zz");
+        int i = 0;
+        i++;
+        System.out.println(i);
     }
 
 }
